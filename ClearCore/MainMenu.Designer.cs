c3864e9ee895862b11197e6723c0e15fef034241
Nekiplay <Nekiplay@DESKTOP-RKN5HA1>
2021-08-15 08:35:26 +0500

    Добавьте файлы проекта.

---
+++ ClearCore/MainMenu.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace ClearCore
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelDesktop = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 45);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Location = new System.Drawing.Point(0, 45);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(800, 405);
+            this.panelDesktop.TabIndex = 1;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelDesktop);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainMenu";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelDesktop;
+    }
+}
+
---
+++ ClearCore/MainMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClearCore
+{
+    public partial class MainMenu : Form
+    {
+        public static PluginsAPI.PluginUpdater PluginUpdater = new PluginsAPI.PluginUpdater();
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+        private Form currentChildForm;
+        private string currentChildFormname;
+        public void OpenChildForm(Form childForm, bool newform = false)
+        {
+            if (currentChildForm != childForm && currentChildFormname != childForm.Name)
+            {
+                if (currentChildForm != null)
+                {
+                    if (newform)
+                    {
+                        currentChildForm.Close();
+                    }
+                    panelDesktop.Controls.Clear();
+                }
+                currentChildForm = childForm;
+                currentChildFormname = childForm.Name;
+                childForm.BackColor = panelDesktop.BackColor;
+                childForm.TopLevel = false;
+                childForm.FormBorderStyle = FormBorderStyle.None;
+                childForm.Dock = DockStyle.Fill;
+                panelDesktop.Controls.Add(childForm);
+                panelDesktop.Tag = childForm;
+                childForm.BringToFront();
+                childForm.Show();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BrokenCoreAPI.UserInfo userInfo = new BrokenCoreAPI.UserInfo();
+            userInfo.Parse("https://brokencore.club/members/1656/");
+            Console.WriteLine(userInfo.Nickname);
+            Console.WriteLine(userInfo.Messages);
+            Console.WriteLine(userInfo.Resources);
+            Console.WriteLine(userInfo.Reactions);
+        }
+    }
+}
